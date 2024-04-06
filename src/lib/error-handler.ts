0f27809d4397b752e141e1dca06dbecab5582c67
Ricardo Racki <ricardo_racki@hotmail.com>
2024-04-06 11:13:42 -0300

    fix: add zodError as client error

--- src/lib/error-handler.ts
+++ src/lib/error-handler.ts
@@ -1,11 +1,11 @@
 import { FastifyInstance } from "fastify";
 import { BadRequest } from "../routes/_errors/bad-request";
+import { ZodError } from "zod";
 type FastifyErrorHandler = FastifyInstance["errorHandler"];
 
 export const errorHandler: FastifyErrorHandler = (error, req, reply) => {
-  if (error instanceof BadRequest) {
+  if (error instanceof BadRequest || error instanceof ZodError) {
     return reply.status(400).send({ message: error.message });
   }
-  if (error)
-    return reply.status(500).send({ message: "Internal server error" });
+  return reply.status(500).send({ message: "Internal server error" });
 };
